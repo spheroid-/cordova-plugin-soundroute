48c725eeeaa41f1f8abb1871bfa4e82522925f49
Henry Jalonen <henry.jalonen@palmu.fi>
2017-11-14 15:05:52 +0200

    Added information about additional configuration needed to change the earpiece volume on Android.

---
+++ src/android/hooks/edit_main_activity.js
@@ -0,0 +1,8 @@
+module.exports = function(context) {
+    console.log("\n\
+    *** IMPORTANT ***\n\
+    \n\
+    To allow changing the earpiece output volume, you need to modify the app MainActivity.java class\n\
+    See " + context.opts.plugin.dir + "/README file\n\
+    ");
+};
